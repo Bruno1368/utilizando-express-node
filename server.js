ed8531cc8f0444059965728bb1c50ae02f047991
Bruno <bruno.luis.999@hotmail.com>
2023-09-14 18:18:02 -0300

    segundo commit

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
+import http from "http";
 
-import http from "http"
+const PORT = 3000;
 
-const PORT = 3000
+const rotas = {
+  "/": "Curso de node.js",
+};
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Curso de Node.js")
+  res.writeHead(200, { "Content-Type": "text/plain" });
+            // notação de colchete aqui para passar uma variável como pripriedade, normalmente usando ponto, ex: rotas.nome
+            //req é um dos parametros, é um objeto com uma propriedade url
+  res.end(rotas[req.url]);
 });
 
 server.listen(PORT, () => {
-    console.log(`Servidor escutando na porta ${PORT}`)
+  console.log(`Servidor escutando na porta ${PORT}`);
 });
