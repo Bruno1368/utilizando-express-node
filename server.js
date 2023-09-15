6c03993d7b630a31edd738c89910f547842ec2dc
Bruno <bruno.luis.999@hotmail.com>
2023-09-15 15:18:38 -0300

    iniciando com o express

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
-import http from "http";
+//import http from "http";
+import app from "./src/app.js";
 
 const PORT = 3000;
 
 const rotas = {
   "/": "Curso de node.js",
+  "/livros": "Livro de programacao",
 };
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-            // notação de colchete aqui para passar uma variável como pripriedade, normalmente usando ponto, ex: rotas.nome
-            //req é um dos parametros, é um objeto com uma propriedade url
-  res.end(rotas[req.url]);
-});
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Servidor escutando na porta ${PORT}`);
 });
