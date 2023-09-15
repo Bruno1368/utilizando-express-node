db6a627ca185429bb38cf840420e50129a7f2de4
Bruno <bruno.luis.999@hotmail.com>
2023-09-15 15:49:03 -0300

    criando a rota livros

--- src/app.js
+++ src/app.js
@@ -2,8 +2,23 @@ import express from "express";
 
 const app = express()
 
+const livros = [
+    {
+        id: 1,
+        titulo: "O senhor dos anéis"
+    },
+    {
+        id: 2,
+        titulo: "A revolução dos bichos"
+    }
+]
+
 app.get("/", (req, res) => {
-    res.status(200).send("Curso de Node.js")
+    res.status(200).send("Curso de Node.js");
+})
+
+app.get("/livros", (req, res) => {
+    res.status(200).json(livros);
 })
 
 
