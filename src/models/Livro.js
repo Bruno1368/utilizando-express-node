4e76fe89838d230936d411af9da40296cb6e12a7
Bruno <bruno.luis.999@hotmail.com>
2023-09-18 17:29:14 -0300

    Criando models e schemas

---
+++ src/models/Livro.js
@@ -0,0 +1,23 @@
+// MODELO É UM OBJETO QUE REPRESENTA UMA COLEÇÃO NA BASE DE DADOS
+
+import mongoose from "mongoose";
+//criação de um novo esquema para adicionar os campos propriedade e o dado do valor da chave
+const livroSchema = new mongoose.Schema(
+  {
+    // criar uma propriedade do tipo objectID, do próprio mongoose
+    id: { type: mongoose.Schema.Types.ObjectId },
+    // require: true pq a propriedade titulo é obrigadtória, não consegue criar um livro sem titulo
+    titulo: { type: String, required: true },
+    editora: { type: String },
+    preco: { type: Number },
+    pagina: { type: Number },
+    //esse é o outro parametro do new mongoose.schema
+  },
+  { versionKey: false }
+);
+// 1 parametro é o modelo livros criado no banco e referenciado como string
+// então é passado o parametro a qual model ose refere e qual o seu schema, esquema
+//por exmeplo, os livros criados pelo schema serão enviados para o modelo livros
+const livro = mongoose.model("livros", livroSchema);
+
+export default livro;
